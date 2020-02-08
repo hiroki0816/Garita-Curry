5bcca0d4acd6d969dc155d460005cbf8801c2c0f
hiroki0816 <hiroki.oosugi@gmail.com>
2020-02-08 23:14:12 +0900

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>がんばるぞ</h1>
+    <p>はじめてのギャッツビー.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/blog/">ブログ見てね</Link>
+    <Link to="/blog2/">こっちもブログやねん</Link>
+  </Layout>
+)
+
+export default IndexPage
